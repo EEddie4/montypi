085407dfb0ddbcfb18ca087cf2e9b51732ec6d2c
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-05-10 16:42:41 -0500

    Separating functions by file

---
+++ choose_option_2.c
@@ -0,0 +1,67 @@
+#include "monty.h"
+
+/**
+ * m_pint - Add a new node at the end of a list
+ *
+ * @stack: address of double linked list
+ * @line_number: number of line
+ *
+ */
+
+void m_pint(stack_t **stack, unsigned int line_number)
+{
+
+}
+
+/**
+ * m_pop - Function that removes the top element of the stack
+ *
+ * @stack: address of double linked list
+ * @line_number: number of line
+ *
+ */
+
+void m_pop(stack_t **stack, unsigned int line_number)
+{
+
+}
+
+/**
+ * m_swap - Function that swaps the top two elements of the stack
+ *
+ * @stack: address of double linked list
+ * @line_number: number of line
+ *
+ */
+
+void m_swap(stack_t **stack, unsigned int line_number)
+{
+
+}
+
+/**
+ * m_add - Function that adds the top two elements of the stack
+ *
+ * @stack: address of double linked list
+ * @line_number: number of line
+ *
+ */
+
+void m_add(stack_t **stack, unsigned int line_number)
+{
+
+}
+
+/**
+ * m_nop - Function that doesn't do anything
+ *
+ * @stack: address of double linked list
+ * @line_number: number of line
+ *
+ */
+
+void m_nop(stack_t **stack, unsigned int line_number)
+{
+
+}
+
