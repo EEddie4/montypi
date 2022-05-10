ed7aeeb028a948af755d08bb4d6a13d8208f37f5
Carlos Soria <soriac367@gmail.com>
2022-05-10 13:00:53 -0500

    Entry point of the program

---
+++ main.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+
+/**
+ * main - entry point, evaluates path name.
+ * @argc: number of arguments.
+ * @argv: array of arguments.
+ * Return: EXIT_SUCCESS, EXIT_FAILURE.
+ */
+int main(int argc, char **argv)
+{
+	FILE *f;
+	char *s = NULL;
+	size_t n, r, i;
+	cmds *head = NULL;
+
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	f = fopen(argv[1], "r");
+	if (!f)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	for (i = 1; (r = getline(&s, &n, f)) != EOF; i++)
+	{
+		s[r - 1] = '\0';
+		if (!command_builder(&head, s, i))
+		{
+			dprintf(STDERR_FILENO, "Error: malloc failed\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+	free(s);
+	fclose(f);
+	return (EXIT_SUCCESS);
+}
