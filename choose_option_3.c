c974b868b45c498e52c424eefb36650ddce504bd
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-05-11 10:59:10 -0500

    Adding mod opcode to computes the rest of the division of the second top element of the stack by the top element of the stack

--- choose_option_3.c
+++ choose_option_3.c
@@ -83,3 +83,37 @@ void m_mul(stack_t **stack, unsigned int line_number)
 	tmp->prev->n *= tmp->n;
 	m_pop(stack, line_number);
 }
+
+
+/**
+ * m_mod - Function that computes the rest of the division of the second
+ * top element of the stack by the top element of the stack.
+ *
+ * @stack: address of double linked list
+ * @line_number: number of line
+ *
+ */
+
+void m_mod(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = *stack;
+	size_t i = 1;
+
+	for (; tmp && tmp->next; tmp = tmp->next, i++)
+		;
+	if (i < 2)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't mod, stack too short\n", line_number);
+		freell(stack);
+		exit(EXIT_FAILURE);
+	}
+	if (!(tmp->n))
+	{
+		dprintf(STDERR_FILENO, "L%d: division by zero\n", line_number);
+		freell(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	tmp->prev->n %= tmp->n;
+	m_pop(stack, line_number);
+}
