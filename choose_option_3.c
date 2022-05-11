c4fb96947c90800d03aaa2b633ceea08223caef8
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-05-11 10:55:33 -0500

    Adding div opcode to divide to numbers

--- choose_option_3.c
+++ choose_option_3.c
@@ -24,3 +24,35 @@ void m_sub(stack_t **stack, unsigned int line_number)
 	tmp->prev->n -= tmp->n;
 	m_pop(stack, line_number);
 }
+
+/**
+ * m_div - Function that divide the top two elements of the stack
+ *
+ * @stack: address of double linked list
+ * @line_number: number of line
+ *
+ */
+
+void m_div(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = *stack;
+	size_t i = 1;
+
+	for (; tmp && tmp->next; tmp = tmp->next, i++)
+		;
+	if (i < 2)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't sub, stack too short\n", line_number);
+		freell(stack);
+		exit(EXIT_FAILURE);
+	}
+	if (!(tmp->n))
+	{
+		dprintf(STDERR_FILENO, "L%d: division by zero\n", line_number);
+		freell(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	tmp->prev->n /= tmp->n;
+	m_pop(stack, line_number);
+}
